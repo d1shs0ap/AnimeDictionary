d6f33e2306efbb4997a10cf27144e22376a9b34b
Valentino Dante <Dante@Valentinos-MacBook-Air.local>
2020-01-12 15:53:00 -0500

    Dictionary Function Done

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
-const mongoose = require('mongoose')
-const session  = require('express-session')
-
-const app = express()
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+// const session  = require('express-session');
+const app = express();
 
 //DB setup
-const db = require('./config/keys').MongoURI //Here is the require call that module.exports is looking for in keys.js
+// const db = require('./config/keys').MongoURI; //Here is the require call that module.exports is looking for in keys.js
 
-mongoose.connect(db, { useNewUrlParser: true})
-.then(() => console.log('MongoDB Connected...')) //.then is a method that exists on Promises and is a mechanism for code synchronization. 
-.catch(err => console.log(err))
+//mongoose.connect(db, { useNewUrlParser: true})
+//.then(() => console.log('MongoDB Connected...')) //.then is a method that exists on Promises and is a mechanism for code synchronization. 
+//.catch(err => console.log(err));
 //Exception handling is the process of responding to the occurrence, during computation, of exceptions – anomalous or 
 //exceptional conditions requiring special processing – often disrupting the normal flow of program execution.
 
 //Bodyparser
+app.use(express.json());
 app.use(express.urlencoded({ extended: false })); //Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 
 
-app.use(expressLayouts)
-app.set('view engine', 'ejs')
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
-app.use('/', require('./routes/index'))
+app.use('/', require('./routes/index'));
 //app.use('/search', require('.routes/search'))
 
 
 //PORT setup
-const PORT = process.env.PORT || 3000
-app.listen(PORT, console.log(`Server started on port ${PORT}`))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 //Routing refers to determining how an application responds to a client request to a particular endpoint, which is a URI (or path) and a specific HTTP request method (GET, POST, and so on).
 //Each route can have one or more handler functions, which are executed when the route is matched.
-
 const express = require("express")
 const JishoApi = require('unofficial-jisho-api')
-
+const KuroshiroAPI = require('kuroshiro')
+const KuromojiAnalyzer = require('kuroshiro-analyzer-kuromoji')
+//const config = require('./node_modules/config'); what does this do?
 const router = express.Router()
 const jisho = new JishoApi();
+const kuroshiro = new KuroshiroAPI();
+const wanakana = require('wanakana');
+
+kuroshiro.init(new KuromojiAnalyzer()).then( () => {
 
 router.get('/', function (req, res) {
     res.render('home', { title: 'The Anime Dictionary' })
@@ -15,23 +20,40 @@ router.post('/', function (req, res){
     res.redirect('/search?word=' + req.body.word)
 })
 
-router.get('/search', function(req, res) {
+router.get('/search', async function(req, res) {
     if(req.query.word){
+        var style = {title: req.query.word + ' - The Anime Dictionary', 
+                    q: req.query.word };
+        // 
        
-    jisho.searchForKanji(req.query.word).then(result => {  
-        console.log("here------" + result.meaning)
+    var japData = await jisho.searchForPhrase(req.query.word);
+                if (japData.data[0]) {
+                    if (wanakana.isKana(req.query.word)){
+                        style.hiragana = wanakana.toRomaji(req.query.word);
+                style.meaning = japData.data//[0].senses[0].english_definitions;
+                res.render('search', style);
+                    } else {
+                style.hiragana = wanakana.toKana(req.query.word);
+                style.meaning = japData.data//[0].senses[0].english_definitions;
+                res.render('search', style);
+                    }
+                } else {
+                    style.hiragana = wanakana.toKana(req.query.word);
+                    style.meaning = false;
+                    res.render('search', style);
+                }
+            
+            
         
-        res.render('search', 
-        {title: req.query.word + ' - The Anime Dictionary', 
-        q: req.query.word, 
-        meaning: result.meaning
-        })});
+    
+    
+    
        /* res.render('search', 
         {title: req.query.word + ' - The Anime Dictionary', 
         q: req.query.word, 
         meaning: means
         }) */
     }
-})
+})})
 
 module.exports = router

55ba362e5d0d5faf9d22ec8583a58cbb458abee6
Matthew Yang <matthew.yang.0618@gmail.com>
2020-01-13 01:51:20 -0500

    giving heroku keys

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = { //module.exports is the object that's actuallly getting returned when another files uses require to call this file
+    MongoURI: 'mongodb+srv://d1shs0ap:matt12345@cluster0-gs5v3.mongodb.net/test?retryWrites=true&w=majority' //URI stands for Unique Resource Identifier
+}
